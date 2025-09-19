f399a9f434e8cabf08038f1fa7509be6827de7dc
Aleksei Khalin <alexeyx0418@gmail.com>
2025-09-19 19:36:58 +0300

    Add models

---
+++ tests/test_nafbase.py
@@ -0,0 +1,10 @@
+import torch
+
+from extrasvls.models.nafbaseline import NAFBaseline
+
+
+def test_unet_shape() -> None:
+    sample = torch.randn((4, 3, 256, 256))
+    nafb = NAFBaseline(3, 16, 3, [[4, 4, 4, 4], [4], [4, 4, 4, 4]], torch.tensor([256, 256]))
+    out = nafb(sample)
+    assert out.shape == (4, 3, 256, 256)
---
+++ tests/test_unet.py
@@ -0,0 +1,10 @@
+import torch
+
+from extrasvls.models.unet import UNet
+
+
+def test_unet_shape() -> None:
+    sample = torch.randn((4, 3, 256, 256))
+    unet = UNet()
+    out = unet(sample)
+    assert out.shape == (4, 1, 256, 256)
